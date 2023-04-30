2d297a05722cf229e2d92897736afb4e1e891fa5
duanmingqiang <964178976@qq.com>
2023-04-30 20:01:26 +0800

    feat:增加球形词云

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,36 @@
     </template>
     
     <!-- 词云特效 -->
-    <word-cloud />
+    <word-cloud :list="worldCloudList"/>
   </div>
 </template>
 
 <script>
+const data = [
+  {label: "十九大精神", value: "111", weight: 288},
+  {label: "两学一做", value: "111", weight: 11},
+  {label: "中华民族", value: "111", weight: 22},
+  {label: "马克思主义", value: "111", weight: 33},
+  {label: "民族复兴", value: "111", weight: 44},
+  {label: "社会主义制度", value: "111", weight: 55},
+  {label: "国防白皮书", value: "111", weight: 66},
+  {label: "民主革命", value: "111", weight: 77},
+  {label: "文化强国", value: "111", weight: 88},
+  {label: "国家主权", value: "111", weight: 99},
+  {label: "武装颠覆", value: "111", weight: 56},
+  {label: "领土完整", value: "111", weight: 66},
+  {label: "安全", value: "111", weight: 77},
+  {label: "从严治党", value: "111", weight: 1},
+  {label: "现代化经济体系", value: "111", weight: 122},
+  {label: "国防动员", value: "111", weight: 33},
+  {label: "信息化战争", value: "111", weight: 44},
+  {label: "局部战争", value: "111", weight: 55},
+  {label: "教育", value: "111", weight: 66},
+  {label: "兵法", value: "111", weight: 77},
+  {label: "一国两制", value: "111", weight: 88},
+  {label: "特色社会主义思想", value: "111", weight: 99},
 
+]
 import Player from './package/player/index.vue'
 import Bpmn from './package/bpmn/index'
 import WordCloud from './package/word-cloud/index.vue'
@@ -26,6 +50,7 @@ export default {
   },
   data() {
     return {
+      worldCloudList: data
     }
   },
   mounted() {
--- src/package/word-cloud/index.vue
+++ src/package/word-cloud/index.vue
@@ -1,60 +1,135 @@
 <template>
-  <div class="word-cloud-container" id="wrap">
-    <a href="#" class="tag">前端学习</a>
-    <a href="#" class="tag">javascript</a>
-    <a href="#" class="tag">css</a>
-    <a href="#" class="tag">html</a>
-    <a href="#" class="tag">jquery</a>
-    <a href="#" class="tag">设计模式</a>
-    <a href="#" class="tag">算法于数据结构</a>
-    <a href="#" class="tag">nodejs</a>
-    <a href="#" class="tag">tcp/ip详解</a>
-    <a href="#" class="tag">http</a>
-    <a href="#" class="tag">计算机网络</a>
-    <a href="#" class="tag">程序员面试宝典</a>
-    <a href="#" class="tag">剑指offer</a>
-    <a href="#" class="tag">你不知道的javascript</a>
-    <a href="#" class="tag">java</a>
-    <a href="#" class="tag">java核心技术</a>
-    <a href="#" class="tag">nodejs实战</a>
-    <a href="#" class="tag">jquery</a>
-    <a href="#" class="tag">vue</a>
-    <a href="#" class="tag">移动端开发</a>
-    <a href="#" class="tag">html5</a>
-    <a href="#" class="tag">nodejs实战</a>
-    <a href="#" class="tag">nodejs实战</a>
+  <div ref="wordCloudContainer" class="word-cloud-container">
+    <span class="word-label" v-for="(wordInfo, index) in worldLabelList" :key="index" :style="setStyle(wordInfo)" > 
+      {{ wordInfo.label }}
+    </span>
   </div>
 </template>
 
 <script>
-// import { Initialization } from './test'
 export default {
-    mounted() {
-        // let tags = document.getElementsByTagName('a' ) ;
-        // let wrap = document.getElementById( 'wrap' );
-        // let options = {
-        //     data: tags,
-        //     container: wrap
-        // }
-        // new Initialization(options);
+  name: '',
+  props: {
+    list: {
+      type: Array,
+      default() {
+        return []
+      }
     }
+  },
+  data() {
+    return {
+      centerX: 0,
+      centerY: 0,
+      radius: 0,
+      speedX: (Math.PI / 180) * 1,
+      speedY: (Math.PI / 180) * 1,
+      worldLabelList: [],
+      timmer: null,
+      speed: 30
+    }
+  },
+  watch: {
+    list: {
+      immediate: true,
+      handler() {}
+    }
+  },
+  computed: {
+  },
+  mounted() {
+    console.log('wordCloudContainer', )
+    this.setInitData()
+    this.init()
+    clearInterval(this.timmer)
+    this.clearTimmer()
+    this.timmer = setInterval(() => {
+      // 刷新位置
+      this.rotateX()
+      this.rotateY()
+
+    }, this.speed);
+  },
+  methods: {
+    // 初始化
+    init() {
+      const length = this.list.length
+      this.worldLabelList = this.list.map((wordInfo, index) => {
+        let split = -1 + (2 * (index + 1) - 1) / length;
+        console.log(index)
+        // 词云列表均匀分配在平面圆弧度
+        const angle = Math.acos(split);
+        // const angle = (Math.PI / 180) * (360 / length) * index
+        // 这块暂时没太理解，看别人这样写的，
+        const angleZ = angle * Math.sqrt(length * Math.PI)
+        //  设置x坐标
+        const xCoordinate = this.centerX + this.radius * Math.sin(angleZ) * Math.cos(angle)
+        // 设置y坐标
+        const yCoordinate = this.centerY + this.radius * Math.sin(angleZ) * Math.sin(angle)
+        // 设置z坐标
+        const zCoordinate = this.radius * Math.cos(angleZ)
+         
+        const item = {
+          label: wordInfo.label,
+          xCoordinate,
+          yCoordinate,
+          zCoordinate,
+          angle,
+          angleZ
+        }
+        return item
+      })
+    },
+    // 设置中心点和半径等基础信息
+    setInitData() {
+      const info = this.$refs.wordCloudContainer.getBoundingClientRect()
+      this.centerX = parseInt(info.width / 2)
+      this.centerY = parseInt(info.height / 2)
+      const minLength = Math.min(info.width, info.height)
+      this.radius = parseInt(minLength / 2)
+    },
+    // 设置词云item位置等css属性
+    setStyle(wordInfo) {
+      return {
+        top: wordInfo.yCoordinate + 'px',
+        left: wordInfo.xCoordinate + 'px',
+        opacity: wordInfo.zCoordinate > 0? 1: 0.3,
+      }
+    },
+    // 清除定时器
+    clearTimmer() {
+      clearInterval(this.timmer)
+    },
+    rotateX() {
+      const cos = Math.cos(this.speedX);
+      const sin = Math.sin(this.speedX);
+      this.worldLabelList.map(wordInfo => {
+        wordInfo.yCoordinate = (wordInfo.yCoordinate - this.centerY) * cos - wordInfo.zCoordinate * sin + this.centerY
+        wordInfo.zCoordinate = wordInfo.zCoordinate * cos + (wordInfo.yCoordinate - this.centerY) * sin
+
+      })
+    },
+    rotateY() {
+      const cos = Math.cos(this.speedY);
+      const sin = Math.sin(this.speedY);
+      this.worldLabelList.map(wordInfo => {
+        wordInfo.xCoordinate = (wordInfo.xCoordinate - this.centerX) * cos - wordInfo.zCoordinate * sin + this.centerX
+        wordInfo.zCoordinate = wordInfo.zCoordinate * cos + (wordInfo.xCoordinate - this.centerX) * sin;
+      })
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-    .word-cloud-container {
-        width: 500px;
-        height: 400px;
-        // background: blue;
-        position: relative;
-        border: 1px solid red;
-        .tag {
-            display: inline-block;
-            position: absolute;
-            height: 50px;
-            line-height: 50px;
-            font-size: 20px;
-            text-decoration: none;
-        }
-    }
+.word-cloud-container {
+  width: 300px;
+  height: 300px;
+  position: relative;
+  .word-label {
+    position: absolute;
+    white-space: nowrap;
+    transform: translate(-50%, -50%);
+  }
+}
 </style>
